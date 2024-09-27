b189a80285227905e5ef86f8d1d4f07f101caa10
Swetha Mallika Gunturu <swetha-gunturu@cookpad.com>
2024-09-27 12:18:22 +0100

    add support to python3.11

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 
 setup(
-    version='0.10.0',
+    version='0.11.0',
     packages=['django_add_default_value'],
     long_description_content_type='text/markdown',
 )
